8e36f30c548601ac3cf816597db5aa4f07d5263a
wanghui_java <wanghui_java@foxmail.com>
2019-02-04 17:12:48 +0800

    配置logback日志

--- src/main/java/my/ssm/o2o/web/AreaController.java
+++ src/main/java/my/ssm/o2o/web/AreaController.java
@@ -2,6 +2,8 @@ package my.ssm.o2o.web;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,9 +15,16 @@ import my.ssm.o2o.dto.Result;
 import my.ssm.o2o.entity.Area;
 import my.ssm.o2o.service.AreaService;
 
+/**  
+ * <p>区域控制器</p>
+ * <p>Date: 2019年2月4日</p>
+ * @author Wanghui    
+ */  
 @Controller
 @RequestMapping("/super_admin")
 public class AreaController {
+    @SuppressWarnings("unused")
+    private Logger logger = LoggerFactory.getLogger(AreaController.class);
     @Autowired
     private AreaService areaService;
     @GetMapping("/list_area")
